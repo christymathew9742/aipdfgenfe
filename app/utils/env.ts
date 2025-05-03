46a5af4773410f00618573ae460c96bc5b1330d7
mchristy-mufin <m.christy@mufin.co.kr>
2025-05-03 16:07:43 +0530

    .env removed

--- app/utils/env.ts
+++
@@ -1,8 +0,0 @@
-export const AZURE_KEY = ''; 
-export const AZURE_REGION = '';
-
-// Ensure variables are loaded
-if (!AZURE_KEY || !AZURE_REGION) {
-  console.error("Missing Azure Speech Service environment variables.");
-  throw new Error("Azure Speech Service credentials are not set!");
-}
