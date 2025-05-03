0cd60eff68cc378aa949972c10482838f65cc788
mchristy-mufin <m.christy@mufin.co.kr>
2025-05-03 16:01:40 +0530

    new version updated

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,5 +1,5 @@
-export const AZURE_KEY = 'aaaaaaaaaaaaa5roJbDTFsVUNwz4hYRnCEUi2nSzS8xUoHqdOrvcgDshcdGw67r51JQQJ99AKACYeBjFXJ3w3AAAYACOGsDdb'; 
-export const AZURE_REGION = 'aaaaaaaaaaaeastus';
+export const AZURE_KEY = ''; 
+export const AZURE_REGION = '';
 
 // Ensure variables are loaded
 if (!AZURE_KEY || !AZURE_REGION) {
